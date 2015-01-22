b04f61de24c585f61e57cf3cdd9ae09d64fdef1e
Lalit Kapoor <lalitkapoor@gmail.com>
2015-01-22 13:58:56 -0600

    docker support

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 react-webpack-es6-sample
 =========
 A sample app in es6 making use of webpack, react, bower, gulp.
-Supports webpack's hot reload for react
+Supports webpack's hot reload for react.
+
+To run with docker:
+
+```bash
+docker build -t react-webpack-es6-sample .
+docker run -d -p 80:80 --name test react-webpack-es6-sample
+open http://$(boot2docker ip) #OSX
+# open http://127.0.0.1 #OSX
+```
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var config = require('./webpack.config')
 var webpack = require('webpack')
 var WebpackDevServer = require('webpack-dev-server')
 
+var port = process.env.PORT || 3000
+
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath
 , contentBase: config.devServer.contentBase
 , hot: true
 , quiet: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(port, function (err, result) {
   if (err) return console.log(err)
-  console.log('Listening at localhost:3000');
+  console.log('Listening at localhost:'+port);
 })
